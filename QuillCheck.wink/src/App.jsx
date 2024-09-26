7ecffe7ad1eec70600f58009a4470a9d0beef28e
nishanthspk <spknishanth2004@gmail.com>
2024-09-26 23:54:20 +0530

    responsive

--- QuillCheck.wink/src/App.jsx
+++ QuillCheck.wink/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <div className="min-h-screen lg:h-screen bg-cover bg-center bricolage-font pb-6 bg-custom-bg jost flex justify-center items-center pt-3">
       <div className="p-3 space-y-5">
-        <img className="w-[200px] h-[40px] mx-auto" src={Assets.QuillCheckLogo} alt="Quill Check Logo" />
+        <img className="w-[200px] lg:h-[40px] mx-auto" src={Assets.QuillCheckLogo} alt="Quill Check Logo" />
 
         {/* AnimatePresence with mode="wait" */}
         <AnimatePresence mode="wait">
