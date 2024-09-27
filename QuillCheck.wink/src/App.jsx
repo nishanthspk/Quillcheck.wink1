2ffa51d8c58f7cbdb2cba2aa4de568a52143f89c
nishanthspk <spknishanth2004@gmail.com>
2024-09-27 14:42:48 +0530

    player

--- QuillCheck.wink/src/App.jsx
+++ QuillCheck.wink/src/App.jsx
@@ -54,9 +54,9 @@ const App = () => {
           Powered by <img className="lg:h-5 h-4 pl-2 mt-1 lg:pl-2" src={Assets.QuillAI} alt="QuillAI" />
         </p>
       </div>
-      <div className='md:block'>
+      
       <TwitterPlayer />
-      </div>
+      
     </div>
   );
 };
